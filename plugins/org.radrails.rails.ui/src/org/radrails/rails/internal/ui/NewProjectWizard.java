996b4eb0ad7d6a7fd205c145d6a88675262a61bc
sgtcoolguy <cwilliams@aptana.com>
2009-11-24 16:48:55 -0500

    Copy BasicNewProjectResourceWizard and strip out the working set and project references code, so that our wizard behaves like the very basic functionality of the generic project wizard.

--- plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/NewProjectWizard.java
+++ plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/NewProjectWizard.java
@@ -1,40 +1,123 @@
 package org.radrails.rails.internal.ui;
 
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
 import java.util.Map;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceStatus;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkingSet;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.dialogs.WizardNewProjectReferencePage;
+import org.eclipse.ui.ide.undo.CreateProjectOperation;
+import org.eclipse.ui.ide.undo.WorkspaceUndoUtil;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.ide.StatusUtil;
+import org.eclipse.ui.internal.wizards.newresource.ResourceMessages;
+import org.eclipse.ui.statushandlers.StatusAdapter;
+import org.eclipse.ui.statushandlers.StatusManager;
 import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
 import org.radrails.rails.core.RailsProjectNature;
 import org.radrails.rails.ui.RailsUIPlugin;
 
 import com.aptana.util.ProcessUtil;
 
-public class NewProjectWizard extends BasicNewProjectResourceWizard
+public class NewProjectWizard extends BasicNewResourceWizard implements IExecutableExtension
 {
+
+	private WizardNewProjectCreationPage mainPage;
+	private WizardNewProjectReferencePage referencePage;
+
+	// cache of newly-created project
+	private IProject newProject;
+
+	/**
+	 * The config element which declares this wizard.
+	 */
+	private IConfigurationElement configElement;
+
+	public NewProjectWizard()
+	{
+		IDialogSettings workbenchSettings = IDEWorkbenchPlugin.getDefault().getDialogSettings();
+		IDialogSettings section = workbenchSettings.getSection("BasicNewProjectResourceWizard");//$NON-NLS-1$
+		if (section == null)
+		{
+			section = workbenchSettings.addNewSection("BasicNewProjectResourceWizard");//$NON-NLS-1$
+		}
+		setDialogSettings(section);
+	}
 
 	@Override
 	public void addPages()
 	{
 		super.addPages();
-		getPage("basicNewProjectPage").setImageDescriptor( //$NON-NLS-1$
-				RailsUIPlugin.imageDescriptorFromPlugin(RailsUIPlugin.getPluginIdentifier(), "icons/newproj_wiz.gif")); //$NON-NLS-1$
+
+		mainPage = new WizardNewProjectCreationPage("basicNewProjectPage");
+		mainPage.setTitle(ResourceMessages.NewProject_title);
+		mainPage.setDescription(ResourceMessages.NewProject_description);
+		this.addPage(mainPage);
+		//
+		//		mainPage.setImageDescriptor( //$NON-NLS-1$
+		//				RailsUIPlugin.imageDescriptorFromPlugin(RailsUIPlugin.getPluginIdentifier(), "icons/newproj_wiz.gif")); //$NON-NLS-1$
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on BasicNewResourceWizard.
+	 */
+	protected void initializeDefaultPageImageDescriptor()
+	{
+		ImageDescriptor desc = RailsUIPlugin.imageDescriptorFromPlugin(RailsUIPlugin.getPluginIdentifier(),
+				"icons/newproj_wiz.gif"); //$NON-NLS-1$
+		setDefaultPageImageDescriptor(desc);
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on IWorkbenchWizard.
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection currentSelection)
+	{
+		super.init(workbench, currentSelection);
+		setNeedsProgressMonitor(true);
+		setWindowTitle(ResourceMessages.NewProject_windowTitle);
 	}
 
 	@Override
 	public boolean performFinish()
 	{
-		boolean valid = super.performFinish();
-		if (!valid)
-			return valid;
+		createNewProject();
 
-		final IProject project = getNewProject();
+		if (newProject == null)
+		{
+			return false;
+		}
+
+		IWorkingSet[] workingSets = mainPage.getSelectedWorkingSets();
+		getWorkbench().getWorkingSetManager().addToWorkingSets(newProject, workingSets);
+
+		updatePerspective();
+		selectAndReveal(newProject);
+
+		final IProject project = newProject;
 		Job job = new Job(Messages.NewProjectWizard_JobTitle)
 		{
 			@Override
@@ -68,4 +151,120 @@ public class NewProjectWizard extends BasicNewProjectResourceWizard
 		return true;
 	}
 
+	/**
+	 * Stores the configuration element for the wizard. The config element will be used in <code>performFinish</code> to
+	 * set the result perspective.
+	 */
+	public void setInitializationData(IConfigurationElement cfig, String propertyName, Object data)
+	{
+		configElement = cfig;
+	}
+
+	/**
+	 * Creates a new project resource with the selected name.
+	 * <p>
+	 * In normal usage, this method is invoked after the user has pressed Finish on the wizard; the enablement of the
+	 * Finish button implies that all controls on the pages currently contain valid values.
+	 * </p>
+	 * <p>
+	 * Note that this wizard caches the new project once it has been successfully created; subsequent invocations of
+	 * this method will answer the same project resource without attempting to create it again.
+	 * </p>
+	 * 
+	 * @return the created project resource, or <code>null</code> if the project was not created
+	 */
+	private IProject createNewProject()
+	{
+		if (newProject != null)
+		{
+			return newProject;
+		}
+
+		// get a project handle
+		final IProject newProjectHandle = mainPage.getProjectHandle();
+
+		// get a project descriptor
+		URI location = null;
+		if (!mainPage.useDefaults())
+		{
+			location = mainPage.getLocationURI();
+		}
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		final IProjectDescription description = workspace.newProjectDescription(newProjectHandle.getName());
+		description.setLocationURI(location);
+
+		// create the new project operation
+		IRunnableWithProgress op = new IRunnableWithProgress()
+		{
+			public void run(IProgressMonitor monitor) throws InvocationTargetException
+			{
+				CreateProjectOperation op = new CreateProjectOperation(description,
+						ResourceMessages.NewProject_windowTitle);
+				try
+				{
+					// see bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=219901
+					// directly execute the operation so that the undo state is
+					// not preserved. Making this undoable resulted in too many
+					// accidental file deletions.
+					op.execute(monitor, WorkspaceUndoUtil.getUIInfoAdapter(getShell()));
+				}
+				catch (ExecutionException e)
+				{
+					throw new InvocationTargetException(e);
+				}
+			}
+		};
+
+		// run the new project creation operation
+		try
+		{
+			getContainer().run(true, true, op);
+		}
+		catch (InterruptedException e)
+		{
+			return null;
+		}
+		catch (InvocationTargetException e)
+		{
+			Throwable t = e.getTargetException();
+			if (t instanceof ExecutionException && t.getCause() instanceof CoreException)
+			{
+				CoreException cause = (CoreException) t.getCause();
+				StatusAdapter status;
+				if (cause.getStatus().getCode() == IResourceStatus.CASE_VARIANT_EXISTS)
+				{
+					status = new StatusAdapter(StatusUtil.newStatus(IStatus.WARNING, NLS.bind(
+							ResourceMessages.NewProject_caseVariantExistsError, newProjectHandle.getName()), cause));
+				}
+				else
+				{
+					status = new StatusAdapter(StatusUtil.newStatus(cause.getStatus().getSeverity(),
+							ResourceMessages.NewProject_errorMessage, cause));
+				}
+				status.setProperty(StatusAdapter.TITLE_PROPERTY, ResourceMessages.NewProject_errorMessage);
+				StatusManager.getManager().handle(status, StatusManager.BLOCK);
+			}
+			else
+			{
+				StatusAdapter status = new StatusAdapter(new Status(IStatus.WARNING, IDEWorkbenchPlugin.IDE_WORKBENCH,
+						0, NLS.bind(ResourceMessages.NewProject_internalError, t.getMessage()), t));
+				status.setProperty(StatusAdapter.TITLE_PROPERTY, ResourceMessages.NewProject_errorMessage);
+				StatusManager.getManager().handle(status, StatusManager.LOG | StatusManager.BLOCK);
+			}
+			return null;
+		}
+
+		newProject = newProjectHandle;
+
+		return newProject;
+	}
+
+	/**
+	 * Updates the perspective for the active page within the window.
+	 */
+	protected void updatePerspective()
+	{
+		BasicNewProjectResourceWizard.updatePerspective(configElement);
+	}
 }
