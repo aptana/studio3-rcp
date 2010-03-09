1692856255bb41347a6c3c2128d8a81eef1cc3be
mxia <mxia@aptana.com>
2010-03-08 17:37:53 -0800

    Fixed default encoding to UTF-8.

--- plugins/com.aptana.radrails.rcp/src/com/aptana/radrails/rcp/preferences/IPreferenceConstants.java
+++ plugins/com.aptana.radrails.rcp/src/com/aptana/radrails/rcp/preferences/IPreferenceConstants.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2005-2009 Aptana, Inc.
+ * Copyright (c) 2005-2010 Aptana, Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,17 +7,18 @@
  * http://www.eclipse.org/legal/epl-v10.html. If redistributing this code,
  * this entire header must remain intact.
  */
-package com.aptana.radrails.rcp.main.preferences;
+package com.aptana.radrails.rcp.preferences;
 
 /**
  * Contains all preferences for the Aptana RCP.
  * 
  * @author Ingo Muschenetz
  */
-public interface IPreferenceConstants {
+public interface IPreferenceConstants
+{
 
-    /**
-     * WORKSPACE_ENCODING_SET
-     */
-    String WORKSPACE_ENCODING_SET = "com.aptana.radrails.rcp.main.WORKSPACE_ENCODING_SET"; //$NON-NLS-1$
+	/**
+	 * WORKSPACE_ENCODING_SET
+	 */
+	public static final String WORKSPACE_ENCODING_SET = "com.aptana.radrails.rcp.WORKSPACE_ENCODING_SET"; //$NON-NLS-1$
 }
