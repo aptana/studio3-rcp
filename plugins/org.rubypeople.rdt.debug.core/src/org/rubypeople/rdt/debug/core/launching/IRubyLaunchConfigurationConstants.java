1d551ed0ad97af1959e941129099c398d68266a2
sgtcoolguy <cwilliams@aptana.com>
2010-03-11 12:15:27 -0500

    add more launch attributes in preparation for VM/program args and user being able to change host to attach debugger to

--- plugins/org.rubypeople.rdt.debug.core/src/org/rubypeople/rdt/debug/core/launching/IRubyLaunchConfigurationConstants.java
+++ plugins/org.rubypeople.rdt.debug.core/src/org/rubypeople/rdt/debug/core/launching/IRubyLaunchConfigurationConstants.java
@@ -17,15 +17,39 @@ public interface IRubyLaunchConfigurationConstants
 	 * <code>rubyExecutable</code>).
 	 */
 	public static final String ID_RUBY_EXECUTABLE = "rubyExecutable";
+
+	public static final String ID_RUBY_APPLICATION = "org.rubypeople.rdt.debug.core.launchConfigurationType";
+
+	public static final String ID_RUBY_SOURCE_LOCATOR = "org.rubypeople.rdt.debug.core.sourceLocator";
+
 	/**
 	 * Launch configuration key. Value is a path to a ruby program. The path is a string representing a full path to a
 	 * ruby program in the workspace.
 	 */
 	public static final String ATTR_FILE_NAME = ID_RUBY_DEBUG_MODEL + ".ATTR_FILE_NAME";
 
-	public static final String ID_RUBY_APPLICATION = "org.rubypeople.rdt.debug.core.launchConfigurationType";
-
-	public static final String ID_RUBY_SOURCE_LOCATOR = "org.rubypeople.rdt.debug.core.sourceLocator";
-
+	/**
+	 * Working directory to use when launching the program.
+	 */
 	public static final String ATTR_WORKING_DIRECTORY = ID_RUBY_DEBUG_MODEL + ".ATTR_WORKING_DIR";
+
+	/**
+	 * Arguments to pass to the Ruby binary.
+	 */
+	public static final String ATTR_VM_ARGUMENTS = ID_RUBY_DEBUG_MODEL + ".ATTR_VM_ARGUMENTS";
+
+	/**
+	 * Arguments to pass to the file we're executing.
+	 */
+	public static final String ATTR_PROGRAM_ARGUMENTS = ID_RUBY_DEBUG_MODEL + ".ATTR_PROGRAM_ARGUMENTS";
+
+	/**
+	 * Hostname of the remote host we're trying to attach the debugger to.
+	 */
+	public static final String ATTR_REMOTE_HOST = ID_RUBY_DEBUG_MODEL + ".ATTR_REMOTE_HOST";
+
+	/**
+	 * Default value for ATTR_REMOTE_HOST: localhost.
+	 */
+	public static final String DEFAULT_REMOTE_HOST = "localhost";
 }
