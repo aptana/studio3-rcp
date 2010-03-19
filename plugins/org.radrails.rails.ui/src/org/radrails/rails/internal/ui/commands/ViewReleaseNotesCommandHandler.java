ae14b61f3d5795801857b14f93caf04b1a90aa28
Andrew Shebanow <ashebanow@gmail.com>
2010-03-19 12:59:06 -0700

    add View Release Notes command to Help > Aptana menu

---
+++ plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/commands/ViewReleaseNotesCommandHandler.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package org.radrails.rails.internal.ui.commands;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import org.radrails.rails.ui.RailsUIPlugin;
+
+/**
+ * @author ashebanow
+ *
+ */
+public class ViewReleaseNotesCommandHandler extends AbstractHandler {
+	private static final String RELEASE_NOTES_URL_STRING = "http://radrails.org/tools/radrails/releasenotes/studio3preview"; //$NON-NLS-1$
+	// TODO: switch to real release notes location once we go public
+	// private static final String RELEASE_NOTES_URL_STRING = "http://aptana.org/tools/studio3/releasenotes"; //$NON-NLS-1$
+	private static URL RELEASE_NOTES_URL;
+
+	static
+	{
+		try
+		{
+			RELEASE_NOTES_URL = new URL(RELEASE_NOTES_URL_STRING);
+		}
+		catch (MalformedURLException e)
+		{
+			RailsUIPlugin.logError(e);
+		}
+	}
+
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		if (RELEASE_NOTES_URL == null)
+		{
+			return null;
+		}
+
+		try
+		{
+			IWorkbenchBrowserSupport support = PlatformUI.getWorkbench().getBrowserSupport();
+
+			if (support.isInternalWebBrowserAvailable())
+			{
+				support.createBrowser(
+						IWorkbenchBrowserSupport.NAVIGATION_BAR | IWorkbenchBrowserSupport.LOCATION_BAR
+								| IWorkbenchBrowserSupport.AS_EDITOR | IWorkbenchBrowserSupport.STATUS,
+						"ViewReleaseNotes", //$NON-NLS-1$
+						null, // Set the name to null. That way the browser tab will display the title of page loaded in the browser.
+						null).openURL(RELEASE_NOTES_URL);
+			}
+			else
+			{
+				support.getExternalBrowser().openURL(RELEASE_NOTES_URL);
+			}
+		}
+		catch (PartInitException e)
+		{
+			RailsUIPlugin.logError(e);
+		}
+
+		return null;
+	}
+
+}
