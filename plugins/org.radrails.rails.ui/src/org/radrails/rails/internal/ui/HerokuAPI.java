0104ec164a16fe9b3e7b9e9252d685d236aa09e2
sgtcoolguy <cwilliams@aptana.com>
2010-05-06 10:09:26 -0400

    Fix authenticate method, needed an Accept header. Add method to grab HerokuAPI using userId and password from ~/.heroku/credentials file if it exists.

--- plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/HerokuAPI.java
+++ plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/HerokuAPI.java
@@ -1,7 +1,9 @@
 package org.radrails.rails.internal.ui;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -15,6 +17,28 @@ import org.radrails.rails.ui.RailsUIPlugin;
 @SuppressWarnings("restriction")
 public class HerokuAPI
 {
+
+	/**
+	 * Authorization header name. Used for Basic Auth over HTTP/S.
+	 */
+	private static final String AUTHORIZATION_HEADER = "Authorization";
+
+	/**
+	 * User agent header name. Used to identify studio as the "client".
+	 */
+	private static final String USER_AGENT = "User-Agent"; //$NON-NLS-1$
+
+	/**
+	 * Content Type Accept header. Tells Heroku we can accept given content types. We default to XML and JSON.
+	 */
+	private static final String ACCEPT_HEADER = "Accept"; //$NON-NLS-1$
+	private static final String ACCEPT_CONTENT_TYPES = "application/xml, application/json"; //$NON-NLS-1$
+
+	/**
+	 * Special API Version header for Heroku. We're coded against v2 right now.
+	 */
+	private static final String HEROKU_API_VERSION_HEADER = "X-Heroku-API-Version"; //$NON-NLS-1$
+	private static final String API_VERSION_NUMBER = "2"; //$NON-NLS-1$
 
 	private String userId;
 	private String password;
@@ -38,29 +62,40 @@ public class HerokuAPI
 
 	public IStatus authenticate()
 	{
+		HttpURLConnection connection = null;
 		try
 		{
-			URL url = new URL("https://api.heroku.com/apps"); // FIXME This isn't working. I always get a 200! //$NON-NLS-1$
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			URL url = new URL("https://api.heroku.com/apps"); //$NON-NLS-1$
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestProperty(USER_AGENT, "Aptana Studio 3.0.0"); // FIXME Grab proper version number
+			connection.setRequestProperty(HEROKU_API_VERSION_HEADER, API_VERSION_NUMBER);
+			connection.setRequestProperty(ACCEPT_HEADER, ACCEPT_CONTENT_TYPES);
+			connection.setUseCaches(false);
+			connection.setAllowUserInteraction(false);
 			String usernamePassword = userId + ":" + password; //$NON-NLS-1$
-			connection.setRequestProperty("Authorization", "Basic " + Base64.encode(usernamePassword.getBytes())); //$NON-NLS-1$ //$NON-NLS-2$
+			connection.setRequestProperty(AUTHORIZATION_HEADER,
+					"Basic " + new String(Base64.encode(usernamePassword.getBytes()))); //$NON-NLS-1$
 			int code = connection.getResponseCode();
-			if (code != 200)
-			{
-				if (code == 401)
-				{
-					return new Status(IStatus.ERROR, RailsUIPlugin.getPluginIdentifier(),
-							Messages.HerokuAPI_AuthFailed_Error);
-				}
+			if (code == HttpURLConnection.HTTP_OK)
+				return Status.OK_STATUS;
 
+			if (code == HttpURLConnection.HTTP_UNAUTHORIZED || code == HttpURLConnection.HTTP_FORBIDDEN)
+			{
 				return new Status(IStatus.ERROR, RailsUIPlugin.getPluginIdentifier(),
-						Messages.HerokuAPI_AuthConnectionFailed_Error);
+						Messages.HerokuAPI_AuthFailed_Error);
 			}
-			return Status.OK_STATUS;
+			// some other response code...
+			return new Status(IStatus.ERROR, RailsUIPlugin.getPluginIdentifier(),
+					Messages.HerokuAPI_AuthConnectionFailed_Error);
 		}
 		catch (Exception e)
 		{
 			return new Status(IStatus.ERROR, RailsUIPlugin.getPluginIdentifier(), e.getMessage(), e);
+		}
+		finally
+		{
+			if (connection != null)
+				connection.disconnect();
 		}
 	}
 
@@ -94,5 +129,51 @@ public class HerokuAPI
 			}
 		}
 		return false;
+	}
+
+	public static HerokuAPI fromCredentials()
+	{
+		File file = getCredentialsFile();
+		BufferedReader reader = null;
+		try
+		{
+			reader = new BufferedReader(new FileReader(file));
+			String userId = reader.readLine();
+			String password = reader.readLine();
+			return new HerokuAPI(userId, password);
+		}
+		catch (Exception e)
+		{
+			RailsUIPlugin.logError(e);
+		}
+		finally
+		{
+			try
+			{
+				if (reader != null)
+					reader.close();
+			}
+			catch (IOException e)
+			{
+				// ignore
+			}
+		}
+		// Return a special HerokuAPI that returns false/error for auth?
+		return new HerokuAPI(null, null)
+		{
+			@Override
+			public IStatus authenticate()
+			{
+				return new Status(IStatus.ERROR, RailsUIPlugin.getPluginIdentifier(),
+						"Unable to get credentials from credential file");
+			}
+
+			@Override
+			public boolean writeCredentials()
+			{
+				// do nothing
+				return false;
+			}
+		};
 	}
 }
