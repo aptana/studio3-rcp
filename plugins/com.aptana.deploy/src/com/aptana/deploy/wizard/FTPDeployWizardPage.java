5592340a48fbbbdc3bd0f7b97d80a599dc868583
mxia <mxia@aptana.com>
2010-05-10 19:07:27 -0700

    Added the initial FTP deployment page.

--- plugins/com.aptana.deploy/src/com/aptana/deploy/wizard/FTPDeployWizardPage.java
+++ plugins/com.aptana.deploy/src/com/aptana/deploy/wizard/FTPDeployWizardPage.java
@@ -5,24 +5,21 @@ import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 
 import com.aptana.deploy.Activator;
 import com.aptana.deploy.Messages;
+import com.aptana.ide.ui.ftp.internal.FTPConnectionPropertyComposite;
+import com.aptana.ide.ui.secureftp.internal.CommonFTPConnectionPropertyComposite;
 
-public class FTPDeployWizardPage extends WizardPage
+@SuppressWarnings("restriction")
+public class FTPDeployWizardPage extends WizardPage implements FTPConnectionPropertyComposite.Listener
 {
 
 	static final String NAME = "FTPDeployment"; //$NON-NLS-1$
 	private static final String ICON_PATH = "icons/ftp.png"; //$NON-NLS-1$
 
-	private Combo protocolCombo;
-	private Text siteName;
+	private FTPConnectionPropertyComposite ftpConnectionComposite;
 
 	protected FTPDeployWizardPage()
 	{
@@ -32,43 +29,55 @@ public class FTPDeployWizardPage extends WizardPage
 	@Override
 	public void createControl(Composite parent)
 	{
-		Composite composite = new Composite(parent, SWT.NULL);
-		composite.setLayout(new GridLayout(2, false));
-		composite.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_FILL | GridData.HORIZONTAL_ALIGN_FILL));
-		setControl(composite);
+		ftpConnectionComposite = new CommonFTPConnectionPropertyComposite(parent, SWT.NONE, null, this);
+		ftpConnectionComposite.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_FILL | GridData.HORIZONTAL_ALIGN_FILL));
+		setControl(ftpConnectionComposite);
 
 		initializeDialogUnits(parent);
+		Dialog.applyDialogFont(ftpConnectionComposite);
 
-		// Actual contents
-		Label siteNameLabel = new Label(composite, SWT.NONE);
-		siteNameLabel.setText(Messages.FTPDeployWizardPage_SiteNameLabel);
-
-		siteName = new Text(composite, SWT.SINGLE | SWT.BORDER);
-
-		Label protocolLabel = new Label(composite, SWT.NONE);
-		protocolLabel.setText(Messages.FTPDeployWizardPage_ProtocolLabel);
-
-		protocolCombo = new Combo(composite, SWT.DROP_DOWN | SWT.READ_ONLY);
-		protocolCombo.add("SFTP"); // TODO Add the available protocols in a more programmatic way? //$NON-NLS-1$
-		protocolCombo.add("FTP"); //$NON-NLS-1$
-		protocolCombo.add("FTPS"); //$NON-NLS-1$
-		protocolCombo.setText("SFTP"); //$NON-NLS-1$
-
-		Group remoteInfoGroup = new Group(composite, SWT.NONE);
-		GridData gd = new GridData();
-		gd.horizontalSpan = 2;
-		remoteInfoGroup.setLayoutData(gd);
-		remoteInfoGroup.setText(Messages.FTPDeployWizardPage_RemoteInfoLabel);
-
-		// TODO Add server hostname, login, etc.
-
-		Dialog.applyDialogFont(composite);
+		ftpConnectionComposite.validate();
 	}
 
 	@Override
 	public IWizardPage getNextPage()
 	{
 		return null;
+	}
+
+	@Override
+	public boolean close()
+	{
+		return false;
+	}
+
+	@Override
+	public void error(String message)
+	{
+		if (message == null)
+		{
+			setErrorMessage(null);
+			setMessage(null);
+		}
+		else
+		{
+			setErrorMessage(message);
+		}
+	}
+
+	@Override
+	public void layoutShell()
+	{
+	}
+
+	@Override
+	public void lockUI(boolean lock)
+	{
+	}
+
+	@Override
+	public void setValid(boolean valid)
+	{
 	}
 
 }
