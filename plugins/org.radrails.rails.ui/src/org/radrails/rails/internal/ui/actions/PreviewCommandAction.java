a0b102abc478a242cf7a3fb6f3c9aec1dc4dff5f
mxia <mxia@aptana.com>
2010-08-19 15:27:45 -0700

    Fixed Studio 3 #878: corrected the enable/disablement of Preview menu item.

---
+++ plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/actions/PreviewCommandAction.java
@@ -0,0 +1,89 @@
+package org.radrails.rails.internal.ui.actions;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.radrails.rails.ui.RailsUIPlugin;
+
+import com.aptana.editor.common.CommonEditorPlugin;
+import com.aptana.editor.common.scripting.commands.TextEditorUtils;
+import com.aptana.scripting.model.AbstractElement;
+import com.aptana.scripting.model.BundleManager;
+import com.aptana.scripting.model.CommandElement;
+import com.aptana.scripting.model.filters.AndFilter;
+import com.aptana.scripting.model.filters.IModelFilter;
+import com.aptana.scripting.model.filters.IsExecutableCommandFilter;
+import com.aptana.scripting.model.filters.ScopeFilter;
+
+/**
+ * @author cwilliams
+ */
+public class PreviewCommandAction extends RailsScriptAction
+{
+
+	private static final String PREVIEW_COMMAND_NAME = "Preview"; //$NON-NLS-1$
+	private static final String RAILS_SCOPE = "source.ruby.rails"; //$NON-NLS-1$
+
+	@Override
+	public void run()
+	{
+		List<IModelFilter> filters = new ArrayList<IModelFilter>();
+		filters.add(new IsExecutableCommandFilter()
+		{
+			@Override
+			public boolean include(AbstractElement element)
+			{
+				if (!super.include(element))
+				{
+					return false;
+				}
+				if (!(element instanceof CommandElement))
+				{
+					return false;
+				}
+				CommandElement node = (CommandElement) element;
+				return node.getDisplayName().equals(PREVIEW_COMMAND_NAME);
+			}
+		});
+
+		IEditorPart textEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		if (textEditor instanceof ITextEditor)
+		{
+			try
+			{
+				ITextEditor abstractThemeableEditor = (ITextEditor) textEditor;
+				IDocument document = abstractThemeableEditor.getDocumentProvider().getDocument(
+						abstractThemeableEditor.getEditorInput());
+				int caretOffset = TextEditorUtils.getCaretOffset(abstractThemeableEditor);
+				// Get the scope at caret offset
+				String contentTypeAtOffset = CommonEditorPlugin.getDefault().getDocumentScopeManager()
+						.getScopeAtOffset(document, caretOffset);
+				filters.add(new ScopeFilter(contentTypeAtOffset));
+			}
+			catch (BadLocationException e1)
+			{
+				RailsUIPlugin.logError(e1);
+			}
+		}
+		else
+		{
+			// If no active editor, "use project root". Assume rails scope.
+			filters.add(new ScopeFilter(RAILS_SCOPE));
+		}
+		CommandElement[] commands = BundleManager.getInstance().getCommands(
+				new AndFilter(filters.toArray(new IModelFilter[filters.size()])));
+		if (commands != null && commands.length > 0)
+		{
+			commands[0].execute();
+		}
+	}
+}
--- plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/commands/PreviewCommandHandler.java
+++ plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/commands/PreviewCommandHandler.java
@@ -3,44 +3,16 @@
  */
 package org.radrails.rails.internal.ui.commands;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.texteditor.ITextEditor;
-import org.radrails.rails.ui.RailsUIPlugin;
-
-import com.aptana.editor.common.CommonEditorPlugin;
-import com.aptana.editor.common.scripting.commands.TextEditorUtils;
-import com.aptana.scripting.model.AbstractElement;
-import com.aptana.scripting.model.BundleManager;
-import com.aptana.scripting.model.CommandElement;
-import com.aptana.scripting.model.filters.AndFilter;
-import com.aptana.scripting.model.filters.IModelFilter;
-import com.aptana.scripting.model.filters.IsExecutableCommandFilter;
-import com.aptana.scripting.model.filters.ScopeFilter;
+import org.radrails.rails.internal.ui.actions.PreviewCommandAction;
 
 /**
  * @author cwilliams
  */
 public class PreviewCommandHandler extends AbstractHandler
 {
-
-	private static final String PREVIEW_COMMAND_NAME = "Preview"; //$NON-NLS-1$
-	private static final String RAILS_SCOPE = "source.ruby.rails"; //$NON-NLS-1$
-
-	/**
-	 * 
-	 */
-	public PreviewCommandHandler()
-	{
-	}
 
 	/*
 	 * (non-Javadoc)
@@ -49,52 +21,7 @@ public class PreviewCommandHandler extends AbstractHandler
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
-
-		List<IModelFilter> filters = new ArrayList<IModelFilter>();
-		filters.add(new IsExecutableCommandFilter()
-		{
-			@Override
-			public boolean include(AbstractElement element)
-			{
-				if (!super.include(element))
-					return false;
-				if (!(element instanceof CommandElement))
-					return false;
-				CommandElement node = (CommandElement) element;
-				return node.getDisplayName().equals(PREVIEW_COMMAND_NAME);
-			}
-		});
-
-		IEditorPart textEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-		if (textEditor instanceof ITextEditor)
-		{
-			try
-			{
-				ITextEditor abstractThemeableEditor = (ITextEditor) textEditor;
-				IDocument document = abstractThemeableEditor.getDocumentProvider().getDocument(
-						abstractThemeableEditor.getEditorInput());
-				int caretOffset = TextEditorUtils.getCaretOffset(abstractThemeableEditor);
-				// Get the scope at caret offset
-				String contentTypeAtOffset = CommonEditorPlugin.getDefault().getDocumentScopeManager()
-						.getScopeAtOffset(document, caretOffset);
-				filters.add(new ScopeFilter(contentTypeAtOffset));
-			}
-			catch (BadLocationException e1)
-			{
-				RailsUIPlugin.logError(e1);
-			}
-		}
-		else
-		{
-			// If no active editor, "use project root". Assume rails scope.
-			filters.add(new ScopeFilter(RAILS_SCOPE));
-		}
-		CommandElement[] commands = BundleManager.getInstance().getCommands(
-				new AndFilter(filters.toArray(new IModelFilter[filters.size()])));
-		if (commands != null && commands.length > 0)
-		{
-			commands[0].execute();
-		}
+		(new PreviewCommandAction()).run();
 		return null;
 	}
 }
