cd32c0b38c4963c039babfcaee093c0043cfca01
sgtcoolguy <cwilliams@aptana.com>
2010-11-15 11:21:35 -0500

    Implement [Studio #1312] Detect Rails generator filepaths and generate URLs to open them in editor

--- plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/hyperlink/RenderPathHyperlinkDetector.java
+++ plugins/org.radrails.rails.ui/src/org/radrails/rails/internal/ui/hyperlink/RenderPathHyperlinkDetector.java
@@ -51,6 +51,7 @@ import com.aptana.workbench.hyperlink.EditorLineHyperlink;
 public class RenderPathHyperlinkDetector implements IHyperlinkDetector
 {
 	private static Pattern RENDERED_VIEW_PATTERN = Pattern.compile("^Rendered\\s+(\\S.+?)\\s+"); //$NON-NLS-1$
+	private static Pattern GENERATOR_CREATED_PATTERN = Pattern.compile("^\\s+create\\s+(\\S.+?)\\s*$"); //$NON-NLS-1$
 
 	public IHyperlink[] detectHyperlinks(String contents)
 	{
@@ -64,6 +65,14 @@ public class RenderPathHyperlinkDetector implements IHyperlinkDetector
 			{
 				filepath = "app/views/" + filepath; //$NON-NLS-1$
 			}
+			return new IHyperlink[] { new EditorLineHyperlink(new Region(start, length), filepath, 0) };
+		}
+		m = GENERATOR_CREATED_PATTERN.matcher(contents);
+		if (m.find())
+		{
+			String filepath = m.group(1);
+			int start = m.start(1);
+			int length = m.end(1) - start;
 			return new IHyperlink[] { new EditorLineHyperlink(new Region(start, length), filepath, 0) };
 		}
 		return new IHyperlink[0];
