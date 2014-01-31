bb224f4da8aa13c5887fc45f00fea5d30eafd5cd
Christopher Williams <cwilliams@appcelerator.com>
2014-01-31 12:08:12 -0500

    Try to fix signing the installer

--- builders/com.aptana.win.installer/windows-nsis/Tools/signtool/signit.bat
+++ builders/com.aptana.win.installer/windows-nsis/Tools/signtool/signit.bat
@@ -1,4 +1,4 @@
 
 
-signtool.exe sign /f /keys/aptanakey2012.pfx /d "Aptana Studio" /du http://www.aptana.com /t http://timestamp.verisign.com/scripts/timestamp.dll /v /p aptkey1010 ..\..\Aptana_Studio_3_Setup.exe
+signtool.exe sign /f C:\keys\aptanakey2012.pfx /d "Aptana Studio" /du http://www.aptana.com /t http://timestamp.verisign.com/scripts/timestamp.dll /v /p iVviOEq5 ..\..\Aptana_Studio_3_Setup.exe
 
